52f2abd3c2a2ae171e315034409844d2f35280e6
AntonVoronezh <pstut@mail.ru>
2022-07-14 16:29:33 +0300

    form

---
+++ sss/src/components/Notes/TheForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="note-form-wrap">
+    <form class="note-form" @submit.prevent="onSubmit">
+      <textarea type="text" required v-model="value" placeholder="" />
+      <button class="btn btnPrimary" type="submit">add new note</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('onSubmit', this.value)
+      console.log(this.value);
+      this.value = "";
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.note-form {
+  max-width: 600px;
+}
+
+.note-form-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+</style>
--- sss/src/pages/homePage.vue
+++ sss/src/pages/homePage.vue
@@ -1,16 +1,23 @@
 <template>
   <p>home pahe</p>
-
+<TheForm @onSubmit="handleSubmit"/>
 </template>
 
 <script>
+import TheForm  from '@/components/Notes/TheForm'
 
 export default {
-
+  components: {TheForm},
   data() {
     return {
 
     };
+
   },
+  methods: {
+    handleSubmit(val) {
+      console.log(val)
+    }
+  }
 };
 </script>
