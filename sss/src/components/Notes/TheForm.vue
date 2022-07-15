8fd52bcd6381f35acee2582299893a86fc17f925
AntonVoronezh <pstut@mail.ru>
2022-07-15 15:18:26 +0300

    add form

--- sss/src/components/Notes/TheForm.vue
+++ sss/src/components/Notes/TheForm.vue
@@ -2,21 +2,25 @@
   <div class="note-form-wrap">
     <form class="note-form" @submit.prevent="onSubmit">
       <textarea type="text" required v-model="value" placeholder="" />
+      <TagList />
       <button class="btn btnPrimary" type="submit">add new note</button>
     </form>
   </div>
 </template>
 
 <script>
+import TagList from "@/components/UI/TagList";
 export default {
+  components: { TagList },
   data() {
     return {
       value: "",
+      tags: ["aaaaa", "bbbbb", "ccccc"],
     };
   },
   methods: {
     onSubmit() {
-      this.$emit('onSubmit', this.value)
+      this.$emit("onSubmit", this.value);
       console.log(this.value);
       this.value = "";
     },
@@ -26,7 +30,13 @@ export default {
 
 <style lang="scss">
 .note-form {
-  max-width: 600px;
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+
+  textarea {
+    margin-bottom: 0;
+  }
 }
 
 .note-form-wrap {
