295527c237e38e8422e8a9458b43f3a450aebaa9
AntonVoronezh <pstut@mail.ru>
2022-07-18 16:26:51 +0300

    vuex getters

--- sss/src/pages/homePage.vue
+++ sss/src/pages/homePage.vue
@@ -1,12 +1,35 @@
 <template>
-  {{ getUsers }}
+ <ul>
+   <li v-for="user in getUsers" :key="user.id">
+     <p>{{user.name}}</p>
+     <p>{{user.isAdmin ? '+++' : '---'}}</p>
+   </li>
+ </ul>
+
+  количсевто = {{getAllUsers.length}}
+
+  <div>
+    {{getUser}}
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      userId: 3
+    }
+  },
   computed: {
+    getAllUsers() {
+      return this.$store.getters.getAllUsers;
+    },
+
     getUsers() {
       return this.$store.getters.getUsers;
+    },
+    getUser() {
+      return this.$store.getters.getUserById(this.userId);
     },
   },
 };
--- sss/src/store/index.js
+++ sss/src/store/index.js
@@ -1,16 +1,22 @@
 import { createStore } from "vuex";
 
 export const store = createStore({
-    state: {
-        users: [
-            {id:1, name: 'aaa', isAdmin: true},
-            {id:2, name: 'bbb', isAdmin: false},
-            {id:3, name: 'ccc', isAdmin: false},
-        ]
+  state: {
+    users: [
+      { id: 1, name: "aaa", isAdmin: true },
+      { id: 2, name: "bbb", isAdmin: false },
+      { id: 3, name: "ccc", isAdmin: false },
+    ],
+  },
+  getters: {
+    getAllUsers(state) {
+      return state.users;
     },
-    getters: {
-        getUsers (state) {
-            return state.users
-        }
-    }
+    getUsers(state) {
+      return state.users.filter((user) => !user.isAdmin);
+    },
+    getUserById: (state) => (id) => {
+      return state.users.find((user) => user.id === id);
+    },
+  },
 });
