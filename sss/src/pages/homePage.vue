97b5d911479e26130917f0ecff21494603ef2e11
AntonVoronezh <pstut@mail.ru>
2022-07-20 15:07:50 +0300

    vuex getters

--- sss/src/pages/homePage.vue
+++ sss/src/pages/homePage.vue
@@ -1,15 +1,24 @@
 <template>
- <ul>
-   <li v-for="user in getUsers" :key="user.id">
-     <p>{{user.name}}</p>
-     <p>{{user.isAdmin ? '+++' : '---'}}</p>
-   </li>
- </ul>
+  <div v-if="isUserLogged">user is logged {{ getUser }}</div>
+  <div v-else>
+    <button @click="handleLoginClick" class="btn btnPrimary">fffff</button>
+  </div>
 
-  количсевто = {{getAllUsers.length}}
+  <br />
+  <br />
+  <br />
+
+  <ul>
+    <li v-for="user in getUsers" :key="user.id">
+      <p>{{ user.name }}</p>
+      <p>{{ user.isAdmin ? "+++" : "---" }}</p>
+    </li>
+  </ul>
+
+  количсевто = {{ getAllUsers.length }}
 
   <div>
-    {{getUser}}
+    {{ getUserById }}
   </div>
 </template>
 
@@ -17,18 +26,28 @@
 export default {
   data() {
     return {
-      userId: 3
-    }
+      userId: 3,
+    };
+  },
+  methods: {
+    handleLoginClick() {
+      this.$store.dispatch("setUser", [123, 123]);
+    },
   },
   computed: {
+    isUserLogged() {
+      return this.$store.getters.isUserLogged;
+    },
     getAllUsers() {
       return this.$store.getters.getAllUsers;
     },
-
+    getUser() {
+      return this.$store.getters.getUser;
+    },
     getUsers() {
       return this.$store.getters.getUsers;
     },
-    getUser() {
+    getUserById() {
       return this.$store.getters.getUserById(this.userId);
     },
   },
